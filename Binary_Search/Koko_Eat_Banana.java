d66d105d744e08241f1b5c5bd8e94cfca06c29f1
abhaybhuyar0709 <abhaybhuyar@gmail.com>
2026-09-11 15:16:18 +0530

    Koko eat banana problem

---
+++ Binary_Search/Koko_Eat_Banana.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+public class Koko_Eat_Banana {
+    public static int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = Arrays.stream(piles).max().getAsInt();
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canFinish(piles, h, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private static boolean canFinish(int[] piles, int h, int speed) {
+        int hours = 0;
+        for (int pile : piles) {
+            hours += Math.ceil((double) pile / speed);
+        }
+        return hours <= h;
+    }
+
+    public static void main(String[] args) {
+        int[] piles = {3, 6, 7, 11};
+        int h = 8;
+        System.out.println("Minimum eating speed: " + minEatingSpeed(piles, h));
+    }
+}
